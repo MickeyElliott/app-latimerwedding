8f05495f7a5f6ce35d8f518ca0466eaa7e992ab3
micke <micke@MARKETSURFME>
2017-06-13 21:52:43 -0700

    Add project files.

---
+++ app-latimerwedding/App_Start/RouteConfig.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace app_latimerwedding
+{
+    public class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            routes.MapRoute(
+                name: "Him",
+                url: "Him/{id}",
+                defaults: new { controller = "Home", action = "Him", id = UrlParameter.Optional }
+            );
+
+            routes.MapRoute(
+                name: "Her",
+                url: "Her/{id}",
+                defaults: new { controller = "Home", action = "Her", id = UrlParameter.Optional }
+            );
+
+            routes.MapRoute(
+                name: "Gallery",
+                url: "Gallery/{id}",
+                defaults: new { controller = "Home", action = "Gallery", id = UrlParameter.Optional }
+            );
+
+            routes.MapRoute(
+                name: "Location",
+                url: "Location/{id}",
+                defaults: new { controller = "Home", action = "Location", id = UrlParameter.Optional }
+            );
+
+            routes.MapRoute(
+                name: "Registry",
+                url: "Registry/{id}",
+                defaults: new { controller = "Home", action = "Registry", id = UrlParameter.Optional }
+            );
+
+            routes.MapRoute(
+                name: "Rsvp",
+                url: "Rsvp/{id}",
+                defaults: new { controller = "Home", action = "Rsvp", id = UrlParameter.Optional }
+            );
+
+
+
+            routes.MapRoute(
+                name: "Default",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
---
+++ app-latimerwedding/Controllers/HomeController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace app_latimerwedding.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Her()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Him()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult Location()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult Registry()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult Rsvp()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult Gallery()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
